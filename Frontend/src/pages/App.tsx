91c389b795dc0a36971e74068e9180e402741d69
Aidanvb5 <152182270+Aidanvb5@users.noreply.github.com>
2024-10-23 13:58:40 +0200

    updates

--- Frontend/src/pages/App.tsx
+++ Frontend/src/pages/App.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home'; // Adjust the path if necessary
-import Login from './Login'; // Adjust the path if necessary
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* Add other routes here */}
+    </Routes>
   );
 };
 
